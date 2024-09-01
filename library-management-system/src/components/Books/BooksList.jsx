fa91a8b8e1bb5069cc4773d110e948adc5f7505e
thanmaisai <dev.thanmaisai@gmail.com>
2024-09-01 14:17:38 +0530

    UI(Frontend): Card Updated

--- library-management-system/src/components/Books/BooksList.jsx
+++ library-management-system/src/components/Books/BooksList.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { useMutation } from '@apollo/client';
 import { BORROW_BOOK, GET_BOOKS } from '../../gqloperations/mutations';
 import { useUser } from '../UserContext';
-import { IconButton, useTheme } from '@mui/material';
+import { IconButton, useTheme, Divider } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UpdateIcon from '@mui/icons-material/Edit';
-import AuthorIcon from '@mui/icons-material/Person';
-import CategoryIcon from '@mui/icons-material/Category';
-import AvailableIcon from '@mui/icons-material/CheckCircle';
-import BorrowedIcon from '@mui/icons-material/Cancel';
 import BorrowIcon from '@mui/icons-material/LibraryBooks';
+import LensIcon from '@mui/icons-material/Lens';
 
 const BooksList = ({ books, role, onDelete, onUpdate }) => {
   const { user, borrowBook } = useUser();
@@ -20,7 +17,7 @@ const BooksList = ({ books, role, onDelete, onUpdate }) => {
 
   const handleBorrow = async (bookId) => {
     try {
-      const { data } = await borrowBookMutation({ variables: { _id: bookId } });
+      await borrowBookMutation({ variables: { _id: bookId } });
       borrowBook(bookId);
     } catch (error) {
       console.error('Error borrowing book:', error);
@@ -32,54 +29,53 @@ const BooksList = ({ books, role, onDelete, onUpdate }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {books.length === 0 ? (
-        <p className="text-center text-lg">
-          No books available
-        </p>
+        <p className="text-center text-lg">No books available</p>
       ) : (
         books.map((book) => (
           <div
             key={book._id}
-            className="rounded-lg shadow-lg overflow-hidden transform transition-transform hover:scale-105"
+            className="relative rounded-lg shadow-lg overflow-hidden"
             style={{
               backgroundColor: theme.palette.background.paper,
               color: theme.palette.text.primary,
-              '--tw-shadow': theme.shadows[1],
+              marginBottom: '20px', 
             }}
           >
-            <img src={book.image} alt={book.title} className="w-full h-48 object-cover" />
+            <img
+              src={book.image}
+              alt={book.title}
+              className="w-full h-60 object-cover" 
+              style={{ marginBottom: '16px', border:'none' }} />
             <div className="p-4">
-              <h2 className="text-2xl font-semibold">{book.title}</h2>
-              <div className="flex items-center mt-2">
-                <AuthorIcon className="mr-2" />
-                <span>Author: <span className="font-medium">{book.author}</span></span>
+              <h2 className="text-lg font-semibold">{book.title}</h2>
+              <div className="mt-2">
+                <p className="text-sm">
+                  Author: <span className="font-medium">{book.author}</span>
+                </p>
+                <p className="text-sm">
+                  Category: <span className="font-medium">{book.category}</span>
+                </p>
+                <p className="text-sm">
+                  Available: <span className="font-medium">{book.available}</span>
+                </p>
+                <p className="text-sm">
+                  Borrowed: <span className="font-medium">{book.borrowed}</span>
+                </p>
               </div>
-              <div className="flex items-center mt-2">
-                <CategoryIcon className="mr-2" />
-                <span>Category: <span className="font-medium">{book.category}</span></span>
-              </div>
-              <div className="flex justify-between items-center mt-4">
-                <div className="flex items-center">
-                  <AvailableIcon className="mr-2" style={{ color: theme.palette.success.main }} />
-                  <span>Available: <span className="font-semibold" style={{ color: theme.palette.success.main }}>{book.available}</span></span>
-                </div>
-                <div className="flex items-center">
-                  <BorrowedIcon className="mr-2" style={{ color: theme.palette.error.main }} />
-                  <span>Borrowed: <span className="font-semibold" style={{ color: theme.palette.error.main }}>{book.borrowed}</span></span>
-                </div>
-              </div>
+              <Divider sx={{ my: 1 }} /> 
               <div className="flex space-x-2 mt-4">
                 {role === 'admin' ? (
                   <>
                     <IconButton
                       onClick={() => onDelete(book._id)}
-                      style={{ backgroundColor: theme.palette.error.main, color: theme.palette.error.contrastText }}
+                      className="p-2 bg-red-500 text-white hover:bg-red-600 transition-colors"
                       aria-label="delete"
                     >
                       <DeleteIcon />
                     </IconButton>
                     <IconButton
                       onClick={() => onUpdate(book)}
-                      style={{ backgroundColor: theme.palette.primary.main, color: theme.palette.primary.contrastText }}
+                      className="p-2 bg-blue-500 text-white hover:bg-blue-600 transition-colors"
                       aria-label="update"
                     >
                       <UpdateIcon />
@@ -88,17 +84,26 @@ const BooksList = ({ books, role, onDelete, onUpdate }) => {
                 ) : (
                   <IconButton
                     onClick={() => handleBorrow(book._id)}
-                    className={`transition-colors ${
+                    className={`p-2 transition-colors ${
                       borrowedBooks.includes(book._id)
-                        ? 'bg-gray-400 text-white cursor-not-allowed'
-                        : `bg-green-500 text-white hover:bg-green-600`
+                        ? 'bg-gray-300 text-gray-600 cursor-not-allowed'
+                        : 'bg-green-500 text-white hover:bg-green-600'
                     }`}
-                    disabled={borrowedBooks.includes(book._id)}
+                    disabled={borrowedBooks.includes(book._id) || book.available === 0}
                     aria-label="borrow"
                   >
                     <BorrowIcon />
                   </IconButton>
                 )}
+              </div>
+              <div
+                className={`absolute bottom-4 right-14 px-2 py-1 text-xs font-semibold rounded-md ${
+                  borrowedBooks.includes(book._id) ? 'bg-red-500 text-white' : 'bg-green-500 text-white'
+                }`}
+                style={{ transform: 'translateX(50%)', display: 'flex', alignItems: 'center' }}
+              >
+                <LensIcon sx={{ fontSize: 'small', marginRight: 0.5 }} />
+                {borrowedBooks.includes(book._id) ? 'Borrowed' : 'Available'}
               </div>
             </div>
           </div>
